31740005c09d5aa36d4b59b60634cb531cd5a845
sonofagl1tch <nolette.ryan@gmail.com>
2018-06-04 13:19:26 -0400

    added logic for creating s3 bucket

--- AWS-Logging/cloudTrail-enable.sh
+++ AWS-Logging/cloudTrail-enable.sh
@@ -3,4 +3,11 @@
 #
 #The create-subscription command creates a trail. You can also use this command to create an Amazon S3 bucket for log file delivery and an Amazon SNS topic for notifications. The create-subscription command also starts logging for the trail that it creates.
 
-aws cloudtrail create-subscription --name=awsdetonatonlab-trail --s3-new-bucket=aws-detonatonlab-1234567890 --s3-prefix=detonation-lab --sns-new-topic=awscloudtrail-detonatonlab-log-deliverytopic
+S3_BUCKET="aws-detonatonlab-1234567890"
+
+#if bucket exists just create subscription, if not, also create bucket
+if aws s3 ls "s3://$S3_BUCKET" 2>&1 | grep -q 'NoSuchBucket'
+then
+  aws cloudtrail create-subscription --name=awsdetonatonlab-trail --s3-prefix=detonation-lab --sns-new-topic=awscloudtrail-detonatonlab-log-deliverytopic
+else
+  aws cloudtrail create-subscription --name=awsdetonatonlab-trail --s3-new-bucket=$S3_BUCKET --s3-prefix=detonation-lab --sns-new-topic=awscloudtrail-detonatonlab-log-deliverytopic
