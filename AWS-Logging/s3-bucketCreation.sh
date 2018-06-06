43ed2007861182fd38fbeae9236df6fba573fb41
sonofagl1tch <nolette.ryan@gmail.com>
2018-06-06 16:55:22 -0400

    modified to create all buckets needed

--- AWS-Logging/s3-bucketCreation.sh
+++ AWS-Logging/s3-bucketCreation.sh
@@ -2,11 +2,15 @@
 #create s3 bucket
 #https://docs.aws.amazon.com/cli/latest/reference/s3api/create-bucket.html
 #
-S3_BUCKET="aws-detonatonlab-1234567890"
+#replace the strings in this array with the names of the s3 buckets you want to create for this
+buckets=(cloudTrail macie vpcflow route53 guardDuty)
 
 #if bucket exists just create subscription, if not, also create bucket
-if aws s3 ls "s3://$S3_BUCKET" 2>&1 | grep -q 'NoSuchBucket'
-then
-  echo "bucket exists already"
-else
-  aws s3api create-bucket --bucket $S3_BUCKET --region us-east-1
+for S3_BUCKET in ${buckets[*]}
+do
+  if aws s3 ls "s3://$S3_BUCKET" 2>&1 | grep -q 'NoSuchBucket'
+  then
+    echo "bucket exists already"
+  else
+    aws s3api create-bucket --bucket $S3_BUCKET --region us-east-1
+done
