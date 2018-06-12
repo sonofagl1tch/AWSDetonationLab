8f2ca3c9aac0fac9a30b81cd017184b471d88908
sonofagl1tch <nolette.ryan@gmail.com>
2018-06-12 08:30:51 -0400

    moved to new folder

---
+++ additionalInstallationScripts/installWazuh.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+# install wazuh server
+# Wazuh documentation - https://documentation.wazuh.com/current/installation-guide/installing-wazuh-server/index.html
+#######################################
+# Install Wazuh server on CentOS/RHEL/Fedora.
+## set up the repository
+cat > /etc/yum.repos.d/wazuh.repo <<\EOF
+[wazuh_repo]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=Wazuh repository
+baseurl=https://packages.wazuh.com/3.x/yum/
+protect=1
+EOF
+## Installing the Wazuh Manager
+yum install wazuh-manager -y -q -e 0
+## check the service status
+#service wazuh-manager status
+chkconfig --add wazuh-manager
+service wazuh-manager start
+#######################################
+# Installing the Wazuh API
+## NodeJS >= 4.6.1 is required in order to run the Wazuh API.
+## add the official NodeJS repository
+#curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+wget https://rpm.nodesource.com/setup_6.x
+bash setup_6.x
+## install NodeJS
+yum install nodejs -y -q -e 0
+## install Python if lower than 2.7
+python --version
+## Install the Wazuh API
+yum install wazuh-api -y -q -e 0
+## check the service status
+chkconfig --add wazuh-api
+service wazuh-api start
+service wazuh-api status
+#######################################
+# Installing Filebeat
+# In a single-host architecture (where Wazuh server and Elastic Stack are installed in the same system), the installation of Filebeat is not needed since Logstash will be able to read the event/alert data directly from the local filesystem without the assistance of a forwarder.
+#######################################
+# Installing Elastic Stack
+## install Oracle Java JRE 8
+curl -Lo jre-8-linux-x64.rpm --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jre-8u172-linux-x64.rpm"
+## install the RPM package using yum
+yum install jre-8-linux-x64.rpm -y -q -e 0
+## Install the Elastic repository and its GPG key
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+cat > /etc/yum.repos.d/elastic.repo << EOF
+[elasticsearch-6.x]
+name=Elasticsearch repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+## Install the Elasticsearch package
+yum install elasticsearch-6.2.4 -y -q -e 0
+## Enable and start the Elasticsearch service
+chkconfig --add elasticsearch
+service elasticsearch start
+#wait until elasticsearch comes up before continuing 
+ES_URL=${ES_URL:-'http://localhost:9200'}
+ES_USER=${ES_USER:-kibana}
+ES_PASSWORD=${ES_PASSWORD:-changeme}
+until curl -u ${ES_USER}:${ES_PASSWORD} -XGET "${ES_URL}"; do
+  >&2 echo "Elastic is unavailable - sleeping for 5 seconds"
+  sleep 5
+done
+>&2 echo "Elastic is up - executing commands"
+## Load the Wazuh template for Elasticsearch:
+curl https://raw.githubusercontent.com/wazuh/wazuh/3.2/extensions/elasticsearch/wazuh-elastic6-template-alerts.json | curl -XPUT 'http://localhost:9200/_template/wazuh' -H 'Content-Type: application/json' -d @-
+service elasticsearch restart
+sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml
+#######################################
+# Install the Logstash package
+yum install logstash-6.2.4 -y -q -e 0
+## Download the Wazuh configuration file for Logstash
+## Local configuration (only in a single-host architecture)
+curl -so /etc/logstash/conf.d/01-wazuh.conf https://raw.githubusercontent.com/wazuh/wazuh/3.2/extensions/logstash/01-wazuh-local.conf
+## Because the Logstash user needs to read the alerts.json file, please add it to OSSEC group by running
+usermod -a -G ossec logstash
+## Follow the next steps if you use CentOS-6/RHEL-6 or Amazon AMI (logstash uses Upstart like a service manager and needs to be fixed, see this bug):
+## Edit the file /etc/logstash/startup.options changing line 30 from LS_GROUP=logstash to LS_GROUP=ossec.
+sed -i 's/LS_GROUP=logstash/LS_GROUP=ossec/' /etc/logstash/startup.options
+## Update the service with the new parameters by running the command /usr/share/logstash/bin/system-install
+/usr/share/logstash/bin/system-install
+## Restart Logstash.
+## force install a SysV init script by running: /usr/share/logstash/bin/system-install /etc/logstash/startup.options sysv as root
+/usr/share/logstash/bin/system-install /etc/logstash/startup.options sysv
+service logstash restart
+## Enable and start the Logstash service
+chkconfig --add logstash
+service logstash start
+#######################################
+# install Kibana
+yum install kibana-6.2.4 -y -q -e 0
+## Install the Wazuh App plugin for Kibana
+## Increase the default Node.js heap memory limit to prevent out of memory errors when installing the Wazuh App. Set the limit as follows
+export NODE_OPTIONS="--max-old-space-size=3072"
+## Install the Wazuh App
+/usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/wazuhapp/wazuhapp-3.2.2_6.2.4.zip
+##  Kibana will only listen on the loopback interface (localhost) by default. To set up Kibana to listen on all interfaces, edit the file /etc/kibana/kibana.yml uncommenting the setting server.host. Change the value to:
+sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml
+## Enable and start the Kibana service
+chkconfig --add kibana
+service kibana start
+#######################################
+# Disable the Elasticsearch repository
+# It is recommended that the Elasticsearch repository be disabled in order to prevent an upgrade to a newer Elastic Stack version due to the possibility of undoing changes with the App.
+sed -i "s/^enabled=1/enabled=0/" /etc/yum.repos.d/elastic.repo
+#######################################
+# set user for wazuh
+cd /var/ossec/api/configuration/auth
+node htpasswd -c user wazuh -b wazuh
+service wazuh-manager restart
+service wazuh-api restart
+#######################################
+#confugure wazuh api 
+#curl -X POST "localhost:9200/.wazuh/wazuh-configuration" -H 'Content-Type: application/json' -d' {"took":0,"timed_out":false,"_shards":{"total":1,"successful":1,"skipped":0,"failed":0},"hits":{"total":1,"max_score":1.0,"hits":[{"_index":".wazuh","_type":"wazuh-configuration","_score":1.0,"_source":{"api_user":"wazuh","api_password":"d2F6dWg=","url":"http://172.16.0.21","api_port":"55000","insecure":"true","component":"API","cluster_info":{"manager":"ip-172-16-0-21.ec2.internal","cluster":"Disabled","status":"disabled"},"extensions":{"audit":true,"pci":true,"oscap":true,"aws":false,"virustotal":false}}}]}}'
+API_PROTOCOL=${API_PROTOCOL:-http}
+API_SERVER=${API_SERVER:-172.16.0.21}
+API_PORT=${API_PORT:-55000}
+API_USER=${API_USER:-wazuh}
+API_PASS=${API_PASS:-wazuh}
+API_PASS_BASE64=$(echo -n ${API_PASS} | base64)
+ES_URL=${ES_URL:-'http://localhost:9200'}
+ES_USER=${ES_USER:-kibana}
+ES_PASSWORD=${ES_PASSWORD:-changeme}
+until curl -u ${ES_USER}:${ES_PASSWORD} -XGET "${ES_URL}"; do
+  >&2 echo "Elastic is unavailable - sleeping for 5 seconds"
+  sleep 5
+done
+>&2 echo "Elastic is up - executing commands"
+sleep 5
+echo -e "\nSetting Wazuh API credentials into the Wazuh Kibana application"
+# The Wazuh Kibana application configuration is the document with the ID 1513629884013, don't change that!
+curl -s -u ${ES_USER}:${ES_PASSWORD} -XPOST "${ES_URL}/.wazuh/wazuh-configuration/1513629884013" -H 'Content-Type: application/json' -H "Accept: application/json" -d'
+{
+    "api_user": "'${API_USER}'",
+    "api_password": "'${API_PASS_BASE64}'",
+    "url": "'${API_PROTOCOL}'://'${API_SERVER}'",
+    "api_port": "'${API_PORT}'",
+    "insecure": "true",
+    "component": "API",
+    "active": true,
+    "extensions": {
+        "oscap": true,
+        "audit": true,
+        "pci": true
+    },
+    "cluster_info": {
+        "node": "wazuh-manager-master",
+        "cluster": "wazuh",
+        "manager": "ip-172-16-0-21.ec2.internal",
+        "status": "enabled"
+    }
+}
+'
+#######################################
+#wait until elasticsearch comes up before continuing 
+ES_URL=${ES_URL:-'http://172.16.0.1:9200'}
+ES_USER=${ES_USER:-kibana}
+ES_PASSWORD=${ES_PASSWORD:-changeme}
+until curl -u ${ES_USER}:${ES_PASSWORD} -XGET "${ES_URL}"; do
+  service elasticsearch restart
+  sleep 5
+done
+>&2 echo "Elastic is up - executing commands"
+#######################################
+# next steps is to configure wazuh
+## https://documentation.wazuh.com/current/installation-guide/installing-elastic-stack/connect_wazuh_app.html
---
+++ additionalInstallationScripts/installWazuhAgent.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# install wazuh server
+# Wazuh documentation - https://documentation.wazuh.com/current/installation-guide/installing-wazuh-agent/wazuh_agent_rpm.html#wazuh-agent-rpm
+#######################################
+#sleep timer for if you want this script to run on instance creation. the server takes 5+ minutes to intall.
+#sleep 10m
+# Adding the Wazuh repository
+cat > /etc/yum.repos.d/wazuh.repo <<\EOF
+[wazuh_repo]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=Wazuh repository
+baseurl=https://packages.wazuh.com/3.x/yum/
+protect=1
+EOF
+
+# Installing Wazuh agent
+yum install wazuh-agent -y -q -e 0
+
+#wait until elasticsearch comes up before continuing 
+ES_URL=${ES_URL:-'http://172.16.0.21:9200'}
+ES_USER=${ES_USER:-kibana}
+ES_PASSWORD=${ES_PASSWORD:-changeme}
+until curl -u ${ES_USER}:${ES_PASSWORD} -XGET "${ES_URL}"; do
+  >&2 echo "Elastic is unavailable - sleeping for 5 seconds"
+  sleep 5
+done
+>&2 echo "Elastic is up - executing commands"
+
+# register agent
+# https://raw.githubusercontent.com/wazuh/wazuh-api/3.2/examples/api-register-agent.sh
+# Connection variables
+API_IP="172.16.0.21"
+API_PORT="55000"
+PROTOCOL="http"
+USER="wazuh"
+PASSWORD="wazuh"
+
+if [ "$#" = "0" ]; then
+  AGENT_NAME=$(hostname)
+else
+  AGENT_NAME=$1
+fi
+
+# Adding agent and getting Id from manager
+echo ""
+echo "Adding agent:"
+echo "curl -s -u $USER:**** -k -X POST -d 'name=$AGENT_NAME' $PROTOCOL://$API_IP:$API_PORT/agents"
+API_RESULT=$(curl -s -u $USER:"$PASSWORD" -k -X POST -d 'name='$AGENT_NAME $PROTOCOL://$API_IP:$API_PORT/agents)
+echo -e $API_RESULT | grep -q "\"error\":0" 2>&1
+
+if [ "$?" != "0" ]; then
+  echo -e $API_RESULT | sed -rn 's/.*"message":"(.+)".*/\1/p'
+  exit 1
+fi
+# Get agent id and agent key
+AGENT_ID=$(echo $API_RESULT | cut -d':' -f 4 | cut -d ',' -f 1)
+AGENT_KEY=$(echo $API_RESULT | cut -d':' -f 5 | cut -d '}' -f 1)
+
+echo "Agent '$AGENT_NAME' with ID '$AGENT_ID' added."
+echo "Key for agent '$AGENT_ID' received."
+
+# Importing key
+echo ""
+echo "Importing authentication key:"
+echo "y" | /var/ossec/bin/manage_agents -i $AGENT_KEY
+
+# fix bad ossec config
+sed -i "s/MANAGER_IP/172.16.0.21/" /var/ossec/etc/ossec.conf
+
+# Restarting agent
+echo ""
+echo "Restarting:"
+echo ""
+/var/ossec/bin/ossec-control restart
+
+exit 0
