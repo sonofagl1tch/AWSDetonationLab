07268c83128599e9a041b824d5e7ef14f925c809
sonofagl1tch <nolette.ryan@gmail.com>
2018-10-05 10:38:58 -0400

    modified append statement

--- additionalInstallationScripts/installWazuh.sh
+++ additionalInstallationScripts/installWazuh.sh
@@ -178,7 +178,7 @@ curl -s -u ${ES_USER}:${ES_PASSWORD} -XPOST "${ES_URL}/.wazuh/wazuh-configuratio
 }
 '
 #######################################
-cat <<EOF >> /var/ossec/etc/ossec.conf
+cat >> /var/ossec/etc/ossec.conf <<EOF
 <ossec_config>
   <wodle name="aws-s3">
     <disabled>no</disabled>
