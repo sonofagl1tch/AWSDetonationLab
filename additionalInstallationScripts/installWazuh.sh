05fdb9969168478ff7b15a3c72535313b515f60a
Ryan Nolete <sonofagl1tch@users.noreply.github.com>
2019-03-04 15:11:40 -0500

    Set wazuh-alerts-3.x-* as Kibana's default index pattern (#64)

--- additionalInstallationScripts/installWazuh.sh
+++ additionalInstallationScripts/installWazuh.sh
@@ -267,5 +267,20 @@ until curl -u ${ES_USER}:${ES_PASSWORD} -XGET "${ES_URL}"; do
 done
 >&2 echo "Elastic is up - executing commands"
 #######################################
+#set default kibana index to wazuh alerts
+K_URL='localhost:5601/api/kibana/settings/defaultIndex'
+K_USER='elastic'
+K_PASSWORD='changeme'
+# wait until kibana service is avilable.
+until curl -u ${K_USER}:${K_PASSWORD} -XGET "localhost:5601"; do
+  sleep 5
+done
+# wait until kibana service is ready
+until [ "$(curl "http://${K_USER}:${K_PASSWORD}@localhost:5601")" != "Kibana server is not ready yet" ]; do
+  sleep 5
+done
+echo "Kibana is up"
+curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" -d '{"value":"wazuh-alerts-3.x-*"}' "http://${K_USER}:${K_PASSWORD}@${K_URL}"
+#######################################
 # next steps is to configure wazuh
 ## https://documentation.wazuh.com/current/installation-guide/installing-elastic-stack/connect_wazuh_app.html
